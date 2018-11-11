0c382ceb338c8d53b7ad8b6f5bdc7c6ff8e22a4e
Heshan Padmasiri <heshanpadmasiri@hotmail.com>
2018-11-11 20:12:41 +0530

    Added code for waterlevel

--- models/waterlevel.js
+++ models/waterlevel.js
@@ -9,8 +9,8 @@ const pool = new sql.ConnectionPool({
   }
 });
 
-function rainfall_query(number_of_units,table,callback){
-  pool.request().query(`SELECT TOP (${number_of_units * 6}) * FROM ${table}`).then((err,result) => {
+function __rainfall_query(number_of_units,table,callback){
+  pool.request().query(`SELECT TOP (${number_of_units * 12}) * FROM ${table}`).then((err,result) => {
     if(err){
       console.error(err);
       return callback(err,null);
@@ -19,6 +19,42 @@ function rainfall_query(number_of_units,table,callback){
       return callback(null,result);
     }
   });
+}
+
+function rainfall_query(number_of_units, table,callback){
+  var stations = [];  
+  var response = {}; 
+  var stationIDs = [51,52,53,54,55,56]
+  for (let index = 0; index < stationIDs.length; index++) {
+    const id = stationIDs[index];
+    stations.push({
+      station_id:id,
+      station_name: `${id}`
+    });
+  }
+  count = 0;
+  stations.forEach(station => {
+    console.log(station)
+    pool.request().query(`SELECT TOP(${number_of_units}) wld_TotFV FROM ${table} WHERE wld_sm_id = ${station.station_id}`).then((r,e)=>{
+      count ++;
+      if(e){
+        console.error(e);
+        return callback(e,null);
+      } else {
+        var temp = {
+          station_name: station.station_name,
+          rainfall:r.recordset,
+          station_id: station.station_id
+        }
+        const id = station.station_id;
+        response[id] = temp;          
+      } 
+      if(count == stations.length){
+        console.log(response);
+        callback(null, response);
+      }
+    });  
+  })
 }
 
 module.exports.get_data = (number_of_units,unit_type,callback) => {    
